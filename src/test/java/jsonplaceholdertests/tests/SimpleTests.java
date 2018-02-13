261c22a56fd47067e3a085011f5666861244ce8f
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-02-13 06:12:14 +0300

    Add logger

--- src/test/java/jsonplaceholdertests/tests/SimpleTests.java
+++ src/test/java/jsonplaceholdertests/tests/SimpleTests.java
@@ -5,8 +5,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import jsonplaceholdertests.entities.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,26 +18,29 @@ public class SimpleTests {
     Gson gson = new Gson();
     RestTemplate restTemplate = new RestTemplate();
     ResponseEntity<User[]> response = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/users", User[].class);
+    Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Test
     public void checkStatusCode() {
         int httpStatus = response.getStatusCodeValue();
+        logger.info("The actual value is " + httpStatus);
         assertEquals(200, httpStatus);
     }
 
     @Test
-    public void checkResponseHeader(){
-       assertNotNull(response.getHeaders().getContentType());
+    public void checkResponseHeader() {
+        MediaType contentType = response.getHeaders().getContentType();
+        logger.info("The value of Content-Type header is " + contentType.toString());
+        assertNotNull(contentType);
     }
 
     @Test
-    public void checkResponseBody(){
+    public void checkResponseBody() {
         User[] users = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users", User[].class);
         JsonArray jsonArray = new JsonParser().parse(gson.toJson(users)).getAsJsonArray();
+        logger.info("The size of JSON array in response is " + jsonArray.size());
         assertEquals(10, jsonArray.size());
     }
-
-
 
 
 }
